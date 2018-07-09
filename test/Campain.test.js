3c4c781c5b5a94ce9cb8655d93323f29f4baed14
munenelewis <munene2379@gmail.com>
2018-07-09 13:54:43 +0300

    emmit listner error

--- test/Campain.test.js
+++ test/Campain.test.js
@@ -7,17 +7,30 @@ const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 
-
 let accounts; //---> a list of all the accounts
 let factory; //----> deployed instance of factory
 let campaignAdress;
 let campaign;
 
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+// increase the limit
+myEmitter.setMaxListeners(11);
+
+for(let i = 0; i < 11; i++) {
+  myEmitter.on('event', _ => console.log(i));
+}
+
+myEmitter.emit('event');
+
 
 beforeEach( async () =>{
     accounts = await web3.eth.getAccounts();
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-    .deploy ({data: CompiledFactory.bytecode})
+    .deploy ({data: compiledFactory.bytecode})
     .send ( { from: accounts[0], gas: 1000000 });
 
     //calls the create campaign function from my contract
