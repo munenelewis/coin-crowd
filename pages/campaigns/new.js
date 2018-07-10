d9c88201126a0468230b6a78e5d65a02ec023873
munenelewis <munene2379@gmail.com>
2018-07-10 10:56:25 +0300

    added a form tag to take in user input

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,19 @@
 import React , {Component} from 'react';
 import Layout from '../../components/layout';
+import {Form , Button, Input} from 'semantic-ui-react';
 
 class CampaignNew extends Component {
     render(){
         return(
         <Layout>
-        <h1> new CampaignNew </h1>
+        <h3> Create a Campaign </h3>
+        <Form>
+            <Form.Field>
+                <label> Minum Contribution </label>
+                <Input label="wei" labelPosition="right" />
+            </Form.Field>
+            <Button primary> Create </Button>
+        </Form>
         </Layout>
         ); 
     }
